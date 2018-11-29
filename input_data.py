52797e2eb15d0e4b9127d4fdd80e52537da42f43
Matthew Normansell <man9ej@virginia.edu>
2018-11-28 22:52:49 -0500

    Data input final

--- input_data.py
+++ input_data.py
@@ -5,35 +5,34 @@ import cv2
 
 # takes image directory and createsr 2D array of immages
 def imageArray(root):
-  
+  #Create the overall data set
+  dataset = []
   #create list of directories in root
   classes = []
   for dir in os.listdir(root):
     classes.append(os.path.join(root, dir))
-
-  # iterate through each directory
-  count = 0
-  file_paths = []
-
-  for file in os.listdir(classes[0]):
-    if count < 20:
-      file_paths.append(os.path.join(classes[0], file))
-      count += 1
-    else: 
-      break
-
-  images = []
-  for path in file_paths:
-    #Create a tuple of the pixel and it's label
-    pixels = cv2.imread(path)
-    height, width = cv2.shape[:2]
-    print('height: %s' % str(height) + ' width: %s' %str(width))
-    images.append( pixels ) 
-
-  images = np.asarray(images)
-  images = images / 255
   
-  return images
+  count = 0
+  for dir in classes:
+  # iterate through each directory
+    file_paths = []
+
+    for file in os.listdir(dir):
+      file_paths.append(os.path.join(dir, file))
+
+    for path in file_paths:
+      if count < 25:
+        #Create a tuple of the pixel and it's label
+        pixels = cv2.imread(path)
+        height, width = pixels.shape[:2]
+        print('file %s | height: %s width: %s' % (path, str(height), str(width)))
+        dataset.append( pixels )
+  
+  return dataset
+    
+
+
+ 
 
   #for dir in classes:
     # iterate through each file in current directory and add it to a file path list
