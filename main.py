c9445e93786f11e95fbe5f628f9894d80c75fae3
Matthew Normansell <man9ej@virginia.edu>
2018-11-30 13:52:29 -0500

    cleaning images

--- main.py
+++ main.py
@@ -2,17 +2,24 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow
+import tensorflow as tf
 import input_data
-from keras.models import Sequential
-from keras.layers import Convolution2D
 
+#Dictionary for labels, unfortunately labels are one less than the pokedex entry
+labels = {
+    '000' : 'Bulbasaur',
+    '001' : 'Ivysaur',
+    '002' : 'Venasaur',
+    '003' : 'Charmander',
+    '004' : 'Charmeleon',
+    '005' : 'Charizard',
+    '006' : 'Squirtle',
+    '007' : 'Wartortle',
+    '008' : 'Blastoise',
+}
 
-dataset = input_data.imageArray('images')
-imgplot = plt.imshow(dataset[0][0])
-plt.show()
+training_iters = 200
+learning_rate = .001
+batch_size = 128
 
-print('First 4 labels {} {} {} {}'.format([dataset[1][1]], dataset[2][1], dataset[3][1], dataset[4][0]))
-
-classes = os.listdir('images')
-num_classes = len(classes)
+# machine learning bit https://www.datacamp.com/community/tutorials/cnn-tensorflow-python
