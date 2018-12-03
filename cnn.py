a96a4778472cfb4258ceb7fa8f91bb8299211dc4
Matthew Normansell <man9ej@virginia.edu>
2018-12-02 21:14:06 -0500

    Getting it to work basically

--- cnn.py
+++ cnn.py
@@ -3,8 +3,8 @@
 
 import tensorflow as tf
 # Importing Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Convolution2D, Dense, Dropout, Activation, Flatten, Convolution2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D, Dense, Dropout, Activation, Flatten, MaxPooling2D
 
 from IPython.display import display
 from PIL import Image
@@ -15,40 +15,46 @@ import pickle
 # Data from preprocessing
 pickle_in = open("X.pickle", "rb")
 X = pickle.load(pickle_in)
+# Rescale rgb values
+X = X/255       
 
 pickle_in = open("Y.pickle", "rb")
 Y = pickle.load(pickle_in)
 
 
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=.333)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
 # Initialize the CNN
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Convolution2D(64, (3, 3), input_shape = X.shape[1:], activation = 'relu'))
 
 # Step 2 - Pooling each 2x2 neighborhood of convoluted image and choosing max
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+# Repeats steps for another layer
+classifier.add(Convolution2D(64, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2,2)))
 
 # Step 3 - Flattening into 1D Array to be able to input into Neural Net
 classifier.add(Flatten())
 
 # Step 4 - Full Connection
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(64))
+classifier.add(Dense(1))
+
+# Step 5 - Activation
+classifier.add(Activation("sigmoid"))
 
 # Compiling CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+classifier.fit(X, Y, batch_size=32, epochs=3, validation_split=.1)
 # Training classifier on training dataset
 # With more epochs, there will be higher accuracy
 # 10 epochs with 8,000 steps/enoch will take 1-2 hours to train
 
 # TODO: split shuffle somehow
 # TODO: we need to feed in the path to a directory containing our training data
-
-classifier.fit_generator(
-        training_set,
-        steps_per_epoch = 500,
-        epochs = 8,
-        validation_data=test_set,
-        validation_steps=800)
