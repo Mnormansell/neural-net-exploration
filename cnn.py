05a14c752a140cd940487c2cff2ad506e5291e7c
Matthew Normansell <man9ej@virginia.edu>
2018-12-22 14:06:02 -0500

    Added testing on test

--- cnn.py
+++ cnn.py
@@ -20,6 +20,7 @@ import time
 import numpy as np
 # Imported to split training data, giving a validating test to help accuracy
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # Im following this https://www.youtube.com/watch?v=WvoLTXIjBYU
 
@@ -79,3 +80,23 @@ model.compile(loss=categorical_crossentropy, optimizer='adam', metrics=['accurac
 # model.summary()
 
 model_training = model.fit(train_x, train_y, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(valid_x, valid_y), callbacks=[tensorboard])
+
+test_eval = model.evaluate(test_x, test_y, verbose=0)
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+predicted_classes = model.predict(test_x)
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+
+from sklearn.metrics import classification_report
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+print(classification_report(test_y, predicted_classes, target_names=target_names))
+
+
+incorrect = np.where(predicted_classes!=test_y)[0]
+print("Found %d incorrect labels" % len(incorrect))
+for i, incorrect in enumerate(incorrect[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(test_x[incorrect].reshape(100,100), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], test_y[incorrect]))
+    plt.tight_layout()
